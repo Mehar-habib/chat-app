29bbcff4775b99e4d77e68bbc9fbac80bc2d96cc
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2023-09-03 14:08:27 +0500

    rename group

--- backend/controller/chatController.js
+++ backend/controller/chatController.js
@@ -96,4 +96,23 @@ const createGroupChat = asyncHandler(async (req, res) => {
   }
 });
 
-export { accessChat, fetchChats, createGroupChat };
+const renameGroup = asyncHandler(async (req, res) => {
+  const { chatId, chatName } = req.body;
+
+  const updatedChat = await Chat.findByIdAndUpdate(
+    chatId,
+    { chatName },
+    { new: true }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!updatedChat) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(updatedChat);
+  }
+});
+
+export { accessChat, fetchChats, createGroupChat, renameGroup };
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -4,13 +4,14 @@ import {
   accessChat,
   createGroupChat,
   fetchChats,
+  renameGroup,
 } from "../controller/chatController.js";
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup)
+router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").delete(protect, removeFromGroup)
 // router.route("/groupradd").delete(protect, addToGroup)
 
