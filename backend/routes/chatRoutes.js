d2f471eb6984fa59a9e726005d11dfbb158e6416
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2023-09-03 13:29:00 +0500

    fetch user chat

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
-import accessChat from "../controller/chatController.js";
+import { accessChat, fetchChats } from "../controller/chatController.js";
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChats)
+router.route("/").get(protect, fetchChats);
 // router.route("/group").post(protect, createGroupChat)
 // router.route("/rename").put(protect, renameGroup)
 // router.route("/groupremove").delete(protect, removeFromGroup)
