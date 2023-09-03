40bd5628d487ff579015b897501a96f1590df211
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2023-09-03 13:58:49 +0500

    create a new group chat

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
-import { accessChat, fetchChats } from "../controller/chatController.js";
+import {
+  accessChat,
+  createGroupChat,
+  fetchChats,
+} from "../controller/chatController.js";
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
-// router.route("/group").post(protect, createGroupChat)
+router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroup)
 // router.route("/groupremove").delete(protect, removeFromGroup)
 // router.route("/groupradd").delete(protect, addToGroup)
