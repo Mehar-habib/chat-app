3762d09ed61639e7aa1e4e0781e6ebb709e647f1
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2023-09-09 14:14:12 +0500

    working chat box and group chat ui

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -1,12 +1,40 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, IconButton, Text } from "@chakra-ui/react";
 import { ChatState } from "../context/ChatProvider";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { getSender, getSenderFull } from "../config/ChatLogics";
+import ProfileModal from "./miscellaneous/ProfileModal";
 
 function SingleChat({ fetchAgain, setFetchAgain }) {
   const { user, selectedChat, setSelectedChat } = ChatState();
   return (
     <>
       {selectedChat ? (
-        <></>
+        <>
+          <Text
+            fontSize={{ base: "28px", md: "30px" }}
+            pb={3}
+            px={2}
+            w="100%"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent={{ base: "space-between" }}
+            alignItems="center"
+          >
+            <IconButton
+              display={{ base: "flex", md: "none" }}
+              icon={<ArrowBackIcon />}
+              onClick={() => setSelectedChat("")}
+            />
+            {!selectedChat.isGroupChat ? (
+              <>
+                {getSender(user, selectedChat.users)}{" "}
+                <ProfileModal user={getSenderFull(user, selectedChat.users)} />{" "}
+              </>
+            ) : (
+              <>{selectedChat.chatName.toUpperCase()} </>
+            )}
+          </Text>
+        </>
       ) : (
         <Box
           display="flex"
